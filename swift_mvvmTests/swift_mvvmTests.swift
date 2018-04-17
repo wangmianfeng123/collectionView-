116a26facc996552218e2a22a1b0b8b2c73341da
蜂尚网络 <fengshang02@fengshangwangluodeMac-mini.local>
2018-04-17 10:20:07 +0800

    Initial Commit

---
+++ swift_mvvmTests/swift_mvvmTests.swift
@@ -0,0 +1,36 @@
+//
+//  swift_mvvmTests.swift
+//  swift_mvvmTests
+//
+//  Created by 蜂尚网络 on 2018/4/17.
+//  Copyright © 2018年 王绵峰. All rights reserved.
+//
+
+import XCTest
+@testable import swift_mvvm
+
+class swift_mvvmTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
